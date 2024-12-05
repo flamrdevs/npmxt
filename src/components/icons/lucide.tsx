4a1c90353cb541f05aad89b15694d86df6dabb21
flamrdevs <flamrdevs@gmail.com>
2024-12-05 08:24:04 +0700

    fix: layering styles

--- src/components/icons/lucide.tsx
+++ src/components/icons/lucide.tsx
@@ -2,8 +2,6 @@ import type { IconNode, IconNodeChild } from 'lucide';
 import { For } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
-import './lucide.css';
-
 export namespace LucideIcon {
 	export type Props = {
 		/**
@@ -17,7 +15,7 @@ const forChild = (child: IconNodeChild) => <Dynamic component={child[0]} {...chi
 
 export const LucideIcon = (props: LucideIcon.Props) => (
 	// biome-ignore lint/a11y/noSvgWithoutTitle: ignore
-	<svg role="img" viewBox="0 0 24 24" class={`ilcd${props.class ? ` ${props.class}` : ''}`}>
+	<svg role="img" viewBox="0 0 24 24" class={`lucide${props.class ? ` ${props.class}` : ''}`}>
 		<For each={props.i[2]}>{forChild}</For>
 	</svg>
 );
